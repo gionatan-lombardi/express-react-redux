1f9ca25e72d4d1016b2b5698970f7dc97666b0af
Gionatan Lombardi <gionatan@vangogh-creative.it>
2017-09-20 12:37:52 +0200

    App.jsx: added react-helmet

--- tests/files/src/client/App.jsx
+++ tests/files/src/client/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 export default () => (
   <div>
+    <Helmet>
+      <title itemProp="name" lang="en">Test Page</title>
+      <meta name="description" content="Helmet application" />
+    </Helmet>
     <h1>Hello</h1>
   </div>
 );
