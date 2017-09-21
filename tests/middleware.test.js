de4db2f4ae53e1dc5611c2f4d782394eacd6cbdd
Gionatan Lombardi <gionatan@vangogh-creative.it>
2017-09-21 12:03:25 +0200

    revert commit fc1aeff25a44eb7e9ce7721aa129d89258e0135a

--- tests/middleware.test.js
+++ tests/middleware.test.js
@@ -5,16 +5,21 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const request = require('request');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const main = require('../lib/main.js');
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 4 * 60 * 1000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 3 * 60 * 1000;
 
 const webpackDevConfig = require('../lib/webpack-dev-config.js');
 
 webpackDevConfig[0].entry[2] = path.join(__dirname, 'files/src/client/index.js');
 webpackDevConfig[1].entry[2] = path.join(__dirname, 'files/src/client/App.jsx');
 webpackDevConfig[2].entry[2] = path.join(__dirname, 'files/src/client/reducer.js');
+webpackDevConfig[0].plugins[1] = new HtmlWebpackPlugin({
+  filename: 'index.html',
+  template: path.join(__dirname, 'files/src/client/index.html'),
+});
 
 describe('middleware unit test', () => {
   it('call it with minimal option', (done) => {
@@ -65,6 +70,7 @@ describe('middleware run test with / route SSR', () => {
     app.use(main({
       webpackDevConfig,
       webpackDevBuildCallback: () => done(),
+      indexSSR: true,
     }));
     server = http.createServer(app);
     server.listen(() => {
@@ -94,12 +100,10 @@ describe('middleware run test with async functions to populate the Store before 
     app.use(main({
       webpackDevConfig,
       webpackDevBuildCallback: () => done(),
+      indexSSR: true,
       beforeSSR: (store, req) => new Promise((resolve) => {
         setTimeout(() => {
-          store.dispatch({
-            type: 'ADD_TODO',
-            todo: `Current path: ${req.url}, Async function resolved 👏`,
-          });
+          store.dispatch({ type: 'ADD_TODO', todo: `Current path: ${req.url}, Async function resolved 👏` });
           resolve();
         }, 1000);
       }),
@@ -113,35 +117,6 @@ describe('middleware run test with async functions to populate the Store before 
   it('get /', (done) => {
     request.get(`http://localhost:${port}/about`, (err, res, body) => {
       expect(body).toContain('Current path: \\u002Fabout, Async function resolved 👏');
-      done();
-    });
-  });
-
-  afterAll((done) => {
-    server.close(done);
-  });
-});
-
-describe('middleware run test with Helmet injecting custom document.title', () => {
-  'use strict';
-
-  let server;
-  let port;
-  beforeAll((done) => {
-    const app = express();
-    app.use(main({
-      webpackDevConfig,
-      webpackDevBuildCallback: () => done(),
-    }));
-    server = http.createServer(app);
-    server.listen(() => {
-      port = server.address().port;
-    });
-  });
-
-  it('get /', (done) => {
-    request.get(`http://localhost:${port}/`, (err, res, body) => {
-      expect(body).toContain('Test Page</title>');
       done();
     });
   });
