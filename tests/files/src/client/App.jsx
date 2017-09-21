3e2d71606b36db198010b918fe5c810caec52325
Gionatan Lombardi <gionatan@vangogh-creative.it>
2017-09-21 12:07:05 +0200

    revert commit 1f9ca25e72d4d1016b2b5698970f7dc97666b0af

--- tests/files/src/client/App.jsx
+++ tests/files/src/client/App.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import Helmet from 'react-helmet';
 
 export default () => (
   <div>
-    <Helmet>
-      <title itemProp="name" lang="en">Test Page</title>
-      <meta name="description" content="Helmet application" />
-    </Helmet>
     <h1>Hello</h1>
   </div>
 );
